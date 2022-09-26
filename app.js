8f2f0637d10b0a35231bff3ec30761ca0fa36132
Rodrigo Silva <rodrigo.silva@catskillet.com>
2022-09-26 16:42:36 -0300

    Crud Users

--- app.js
+++ app.js
@@ -12,7 +12,75 @@ app.get('/', (req, res) => {
     })
 })
 
+//CRUD - C(reate) R(ead) U(pdate) D(elete)
 
+// create
+app.post('/users/create', (req, res) => {
+    
+    let id = 0 ;
+    const lastId = data[data.length - 1 ];
+
+    if(lastId){
+        id = lastId.id + 1
+    }
+
+    if(req.body){
+        data.push({
+            id,
+            ...req.body
+        })
+
+        res.json({
+            status: 200 / "Deu bom",
+            data
+        })
+    }
+
+    res.json({
+        status: 401,
+        message: "Tá faltando coisa ein"
+    })
+
+})
+
+// read
+app.get('/users/read', (req, res) => {
+    res.json({
+        status: 200 / "Deu bom",
+        users: data
+    })
+})
+
+
+// update
+app.put('/users/update', (req,res) => {
+    const newInfo = req.body;
+
+    data = data.map((user) => {
+        if( user.id == newInfo.id ) {
+            return newInfo
+        } 
+
+        return user
+    })
+
+    res.json({
+        status: 200 / "Deu bom",
+        data
+    })
+})
+
+// delete
+app.delete('/user/delete', (req, res) => {
+    data = data.filter((users) => {
+        return users.id !== req.body.id
+    })
+
+    res.json({
+        status: 200 / "Deu bom",
+        data
+    })
+})
 
 
 app.listen(8080, () => {
